057fd2c6a23c1a7bd82018790db8bd9ce535cff4
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-16 12:33:53 -0500

    Add catch to fetch and add POST call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,23 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
+      .catch(err => console.error(err))
+}
+
+export const postUrl = (url, title) => {
+  const body = {
+    long_url: url,
+    title
+  }
+
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
 }
